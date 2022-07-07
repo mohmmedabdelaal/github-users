61aa8e3d9a570915059035723507a1225aac29c6
Moelaal <medo.elaal@gmail.com>
2022-07-07 16:41:11 +0200

    most popular langues

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,31 +1,55 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useGithubContext } from '../context/context';
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+import { Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
   const { repos } = useGithubContext();
-  const chartData = [
-    {
-      label: 'HP',
-      value: '20',
+  const languages = repos.reduce((total, item) => {
+    const { language, stargazers_count } = item;
+    if (!language) return total;
+    if (!total[language]) {
+      total[language] = { label: language, value: 1, stars: stargazers_count };
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+        stars: total[language].stars + stargazers_count,
+      };
+    }
+
+    return total;
+  }, {});
+  const mostUsed = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5);
+
+  const mostPopular = Object.values(languages)
+    .sort((a, b) => {
+      return b.stars - a.stars;
+    })
+    .slice(0, 5);
+  console.log(languages);
+
+  let { stars, forks } = repos.reduce(
+    (total, item) => {
+      const { stargazers_count, name, forks } = item;
+      total[stargazers_count] = { label: name, value: stargazers_count };
+      total.forks[forks] = { label: name, value: forks };
+      return total;
     },
-    {
-      label: 'lenovo',
-      value: '40',
-    },
-    {
-      label: 'dell',
-      value: '40',
-    },
-  ];
+    { stars: {}, forks: {} }
+  );
+  forks = Object.values(forks).slice(-5).reverse();
+  stars = Object.values(stars).slice(-5).reverse();
   return (
     <section>
       <Wrapper className="section-center">
-        {/* <ExampleChart data={chartData} /> */}
-        <Pie3D data={chartData} />
-        <Column3D data={chartData} />
-        <Doughnut2D data={chartData} />
-        <Bar3D data={chartData} />
+        <Pie3D data={mostUsed} />
+        <Column3D data={stars} />
+        <Doughnut2D data={mostPopular} />
+        <Bar3D data={forks} />
       </Wrapper>
     </section>
   );
