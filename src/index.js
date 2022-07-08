c59c4ac3f28264b530e2f7a552c10aff434b8f27
Moelaal <medo.elaal@gmail.com>
2022-07-08 03:19:23 +0200

    adding cachelocation

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ ReactDOM.render(
     domain={process.env.REACT_APP_AUTH_DOMAIN}
     clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
     redirectUri={window.location.origin}
+    cacheLocation="localstorage"
   >
     <GithubProvider>
       <Router>
